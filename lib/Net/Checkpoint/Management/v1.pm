384845122501020e6a7c7295bdfa397fc62245fa
Alexander Hartmaier <abraxxa@cpan.org>
2025-01-20 12:24:52 +0100

    develop and test against version R81.20

--- lib/Net/Checkpoint/Management/v1.pm
+++ lib/Net/Checkpoint/Management/v1.pm
@@ -41,7 +41,7 @@ no warnings "experimental::signatures";
 =head1 DESCRIPTION
 
 This module is a client library for the Checkpoint Management API version 1.x.
-Currently it is developed and tested against version R81.10.
+Currently it is developed and tested against version R81.20.
 
 =cut
 
